05430df473cbc265057dd0d9e02073ff75bb0618
erastus-murungi <erastuswagwan@gmail.com>
2023-04-27 13:49:00 -0400

    cleaned up lalr1 gen

--- gen/lalr1_gen.py
+++ gen/lalr1_gen.py
@@ -16,25 +16,6 @@ GENERATED_PARSER_FILE_NAME = "parser_generated.py"
 
 
 def gen_parser(grammar: Grammar):
-    tokenizer = grammar.tokenizer
-    parser_template_file_path = os.path.join(TEMPLATE_DIR, TEMPLATE_FILENAME)
-    parser_generated_file_path = os.path.join(OUTPUT_DIR, GENERATED_PARSER_FILE_NAME)
-
-    with open(parser_template_file_path, "r") as f:
-        temp = f.read()
-        temp = temp.replace(
-            '"%patterns%"',
-            f"{{{', '.join(f'{identifier!r} : re.compile({pattern.pattern!r}, re.DOTALL)' for identifier, pattern in tokenizer.patterns.items())}}} ",
-        )
-        temp = temp.replace('"%filename%"', repr(tokenizer.get_filename()))
-        temp = temp.replace('"%reserved%"', repr(tokenizer.reserved))
-
-        with open(parser_generated_file_path, "w") as f1:
-            f1.write(temp)
-    try:
-        subprocess.run(["black", parser_generated_file_path])
-    except FileNotFoundError:
-        print("Black not found, skipping formatting")
     parsing_table = LALR1ParsingTable(grammar)
     states = [state.id for state in parsing_table.states]
     states.sort()
@@ -59,24 +40,43 @@ def gen_parser(grammar: Grammar):
                 if isinstance(symbol, Terminal):
                     expected_tokens[state.id].append(symbol.name)
 
-    with open(parser_generated_file_path, "r") as f:
-        temp = f.read()
-        temp = temp.replace('"%parsing_table%"', pretty_repr(simplified_table))
-        temp = temp.replace('"%states%"', pretty_repr(states))
-        temp = temp.replace('"%expected_tokens%"', pretty_repr(dict(expected_tokens)))
+    tokenizer = grammar.tokenizer
 
-        with open(parser_generated_file_path, "w") as f1:
-            f1.write(temp)
+    parser_template_file_path = os.path.join(TEMPLATE_DIR, TEMPLATE_FILENAME)
+    parser_generated_file_path = os.path.join(OUTPUT_DIR, GENERATED_PARSER_FILE_NAME)
+
+    with open(parser_template_file_path, "r") as template, open(
+        parser_generated_file_path, "w"
+    ) as generated:
+        items = (
+            f"{identifier!r} : re.compile({pattern.pattern!r}, re.DOTALL)"
+            for identifier, pattern in tokenizer.patterns.items()
+        )
+
+        generated.write(
+            template.read()
+            .replace(
+                '"%patterns%"',
+                f"{{{', '.join(items)}}} ",
+            )
+            .replace('"%filename%"', repr(tokenizer.get_filename()))
+            .replace('"%reserved%"', repr(tokenizer.reserved))
+            .replace('"%parsing_table%"', pretty_repr(simplified_table))
+            .replace('"%states%"', pretty_repr(states))
+            .replace('"%expected_tokens%"', pretty_repr(dict(expected_tokens)))
+        )
     try:
         subprocess.run(["black", parser_generated_file_path])
     except FileNotFoundError:
         print("Black not found, skipping formatting")
 
-    with open(parser_generated_file_path, "r") as f:
-        temp = f.read()
-        temp = temp.replace("%id%", HMAC(b"key", temp.encode(), "sha256").hexdigest())
-    with open(parser_generated_file_path, "w") as f:
-        f.write(temp)
+    with open(parser_generated_file_path, "r") as generated:
+        generated_str = generated.read()
+
+    with open(parser_generated_file_path, "w") as generated:
+        generated.write(
+            generated_str.replace("%id%", HMAC(b"key", generated_str.encode(), "sha256").hexdigest())
+        )
 
 
 def generate_files(grammar_str: str):
